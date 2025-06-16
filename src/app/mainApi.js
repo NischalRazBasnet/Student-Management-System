49cbf4ca532a7c7b154bab8021f5166b01c9d69c
Nischal <nischalbasnet7848@gmail.com>
2025-06-16 14:00:11 +0545

    url

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { getFromLocal } from '../features/local/local';
 
-export const baseUrl = 'http://192.168.1.71:5000';
+export const baseUrl = 'https://student-management-system-1jdj.onrender.com';
 
 export const mainApi = createApi({
   reducerPath: 'mainApi',
